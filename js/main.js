2732e9fb7bee9de8acbb69a8543d84230c62b5d1
AndTaran <65190142+AndTaran@users.noreply.github.com>
2020-11-23 13:49:38 +0300

    feat: add adaptive and burger-menu

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+const sidebarToggleBtn = document.querySelector('.menu-icon-wrapper');
+const menuIcon = document.querySelector('.menu-icon');
+const sidebar = document.querySelector('.sidebar');
+
+sidebarToggleBtn.onclick = function () {
+   menuIcon.classList.toggle('menu-icon-active');
+   sidebar.classList.toggle('sidebar-active');
+}
